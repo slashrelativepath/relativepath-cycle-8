9513b1cc8ce08b0ef7cabd5fee10c46f8c719844
Bianca <xolotl.tonatiuh@gmail.com>
2024-04-04 15:25:35 -0700

    Added homebrew instal and git

--- setup-darwin.sh
+++ setup-darwin.sh
@@ -1,4 +1,20 @@
 # local setup 
+if (which brew)
+then
+  echo "brew already installed"
+else
+  echo "installing brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+if (which git)
+then
+  echo "git already installed"
+else
+  echo "installing git"
+  brew install git
+fi
+
 if (which nano)
 then
   echo "nano already installed" 
