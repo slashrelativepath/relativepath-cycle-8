e6e44142460c7d4ee3324bcf3f7788ba3646c020
Christopher Forti <christopher.forti.79@gmail.com>
2024-04-07 14:19:20 -0400

    Made brew install non-interactive, and added comments for clarification of code blocks.

--- setup-darwin.sh
+++ setup-darwin.sh
@@ -1,12 +1,14 @@
-# local setup 
+# local setup starting with pkg manager code block 
 if (which brew)
 then
   echo "brew already installed"
 else
   echo "installing brew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  /bin/bash -y -c "$(curl -fsSL 
+https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
+# code bock to install git if not already
 if (which git)
 then
   echo "git already installed"
@@ -15,6 +17,7 @@ else
   brew install git
 fi
 
+# code bock to install nano if not already
 if (which nano)
 then
   echo "nano already installed" 
