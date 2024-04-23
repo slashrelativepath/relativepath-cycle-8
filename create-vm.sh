93b506fe98d104e1e5eeb140c46333c15497b174
Alberto Castro <jaycastro100@gmail.com>
2024-04-23 18:27:08 -0400

    Added exec to deploy the web server

--- create-vm.sh
+++ create-vm.sh
@@ -5,3 +5,11 @@ else
     echo "creating relativepath vm"
     multipass launch --name relativepath
 fi
+
+if (multipass exec relativepath -- curl localhost)
+then
+  echo "webserver already deployed"
+else
+  echo "deploying web server"
+  multipass exec relativepath -- sudo apt install -y nginx
+fi
