c8e96be5d97f52cf9780d80d6416f9f688b173c4
HarmonyMorse <harmonythedev@gmail.com>
2024-04-24 16:01:14 -0700

    WIP Adding ssh key from GitHub

--- create-vm.sh
+++ create-vm.sh
@@ -1,3 +1,11 @@
+if (test -f ./id_ed25519)
+then
+  echo "ssh keypair already exists"
+else
+  echo "creating ssh keypair"
+  ssh-keygen -t ed25519 -f ./id_ed25519 -N ''
+fi
+
 if (multipass info relativepath)
 then
     echo "relativepath vm already exist"
@@ -12,4 +20,12 @@ then
 else
   echo "deploying web server"
   multipass exec relativepath -- sudo apt install -y nginx
+fi
+
+if (ssh ubuntu@$(multipass info relativepath | grep IPv4 | awk '{ print $2 }') 'exit')
+then
+  echo "able to connect using ssh"
+else
+  echo "adding ssh pub key to VM from GitHub"
+  multipass exec relativepath -- bash -c "curl https://github.com/harmonymorse.keys >> .ssh/authorized_keys"
 fi
